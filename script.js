59a256e04d70d45bb9681358ef372684c0a27c92
Miguel Teixeira <miguelt007@gmail.com>
2025-09-24 10:42:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const map = L.map('map').setView([25, -70], 4);
+const map = L.map('map').setView([20, -60], 3);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Exemplo de furacão ativo (substitui por outro se necessário)
-const geojsonUrl = "https://www.nhc.noaa.gov/gis/forecast/archive/AL052025_5day_latest.geojson";
+const tbody = document.querySelector("#stormTable tbody");
+
+// Fonte oficial NOAA/NWS GeoHub
+const geojsonUrl = "https://opendata.arcgis.com/datasets/9e1c3f8c8b5f4b0b9c1f6e6b2f1f3e6e_0.geojson";
 
 fetch(geojsonUrl)
   .then(res => res.json())
@@ -12,21 +14,30 @@ fetch(geojsonUrl)
     L.geoJSON(data, {
       onEachFeature: function (feature, layer) {
         const props = feature.properties;
-        const popup = `
-          <strong>${props.STORMNAME || "Furacão"}</strong><br>
-          Tipo: ${props.STORMTYPE || "Desconhecido"}<br>
-          Data: ${props.ADVDATE || "N/A"}
+        const name = props.STORMNAME || "Desconhecido";
+        const type = props.STORMTYPE || "Tropical";
+        const lat = props.LAT || 0;
+        const lon = props.LON || 0;
+        const wind = props.WINDSPEED || "N/A";
+        const pressure = props.PRESSURE || "N/A";
+
+        // Adiciona à tabela
+        const row = tbody.insertRow();
+        row.innerHTML = `
+          <td>${name}</td>
+          <td>${type}</td>
+          <td>${lat.toFixed(2)}, ${lon.toFixed(2)}</td>
+          <td>${wind}</td>
+          <td>${pressure}</td>
         `;
-        layer.bindPopup(popup);
-      },
-      style: {
-        color: "red",
-        weight: 2,
-        opacity: 0.8
+
+        // Adiciona ao mapa
+        L.marker([lat, lon]).addTo(map)
+          .bindPopup(`<strong>${name}</strong><br>Tipo: ${type}<br>Vento: ${wind} km/h<br>Pressão: ${pressure} hPa`);
       }
     }).addTo(map);
   })
   .catch(err => {
-    console.error("Erro ao carregar GeoJSON:", err);
-    alert("Não foi possível carregar os dados do furacão.");
+    console.error("Erro ao carregar dados:", err);
+    alert("Não foi possível carregar os dados dos furacões.");
   });
